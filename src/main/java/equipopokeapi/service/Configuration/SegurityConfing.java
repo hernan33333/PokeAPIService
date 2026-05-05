c35f63e5b8ba5d87adf2f64054d10bf9c693abbe
AnaKaren830 <anakarengb2002@outlook.com>
2026-05-05 17:02:33 -0600

    TokenLogin

--- src/main/java/equipopokeapi/service/Configuration/SegurityConfing.java
+++ src/main/java/equipopokeapi/service/Configuration/SegurityConfing.java
@@ -5,31 +5,49 @@
 
 package equipopokeapi.service.Configuration;
 
+import equipopokeapi.service.ConfigurationFilter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SegurityConfing {
+    @Autowired
+    private JwtFilter jwtFilter;
     
-@Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    http
-        .csrf(csrf -> csrf.disable()) 
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/auth/**").permitAll()
-            .anyRequest().permitAll()
-        );
+        return http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> 
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
 
-    return http.build();
-}
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+            .build();
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+  @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
--- src/main/java/equipopokeapi/service/Service/JwtService.java
+++ src/main/java/equipopokeapi/service/Service/JwtService.java
@@ -5,21 +5,62 @@
 
 package equipopokeapi.service.Service;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
 import java.util.Date;
+import io.jsonwebtoken.Jwts;
+import java.util.HashMap;
+import java.util.Map;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.function.Function;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    private final String SecretKey = "jbdiuwoklmlmslzmxo90oiokmdsomJhfbdsjknpoPPHIBJlpij";
+    private final String SecretKey = "jbdiuwoklmlmslzmxo90oiokmdsomJhfbdsjknpoPPHIBJlpij";    
+    private final long EXPIRATION_TIME = 86400000;
     
-  public String generateToken(String email) {
-     return Jwts.builder()
-          .setSubject(email)
-     .setIssuedAt(new Date())
-               .setExpiration(new Date(System.currentTimeMillis() + 86400000))        
-             .signWith(SignatureAlgorithm.HS256, SecretKey)
-            .compact();
-  }
+  public String generateToken(String email, Integer idRol) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("rol", idRol);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SecretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+    public Integer extractRol(String token) {
+        return extractClaim(token, claims -> claims.get("rol", Integer.class));
+    }
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        final Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        final String username = extractUsername(token);
+        return (username.equals(email) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+private Claims extractAllClaims(String token) {
+    return Jwts.parser()
+            .setSigningKey(SecretKey)
+            .parseClaimsJws(token)
+            .getBody();
+}
+
 }
